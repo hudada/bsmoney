07ccb5b1d4210b40369605200734bc39804f4011
ote <wdxc1992@qq.com>
2018-03-03 17:10:12 +0800

    main

--- app/src/main/java/com/example/bsproperty/ui/MainActivity.java
+++ app/src/main/java/com/example/bsproperty/ui/MainActivity.java
@@ -1,25 +1,22 @@
 package com.example.bsproperty.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.bsproperty.MyApplication;
 import com.example.bsproperty.R;
 import com.example.bsproperty.fragment.Fragment02;
-import com.example.bsproperty.fragment.Fragment03;
+import com.example.bsproperty.fragment.Fragment01;
 import com.example.bsproperty.fragment.Fragment04;
-import com.example.bsproperty.fragment.HomeFragment;
 import com.example.bsproperty.utils.SpUtils;
 
 import java.util.ArrayList;
@@ -39,29 +36,31 @@ public class MainActivity extends BaseActivity {
     @BindView(R.id.nv_view)
     NavigationView nvView;
 
-    private TextView tv_01, tv_02, tv_name;
+    private TextView tv_01, tv_02,tv_03, tv_name;
 
     private long backTime;
-    private HomeFragment homeFragment;
+    private HomeActivity homeFragment;
     private Fragment02 fragment02;
-    private Fragment03 fragment03;
+    private Fragment01 fragment01;
     private Fragment04 fragment04;
     private ArrayList<Fragment> fragments;
     private MyFragmentPagerAdapter adapter;
     private String[] tabs = new String[]{
-            "1", "2", "3"
+            "账单", "图表", "发现"
     };
 
     @Override
     protected void initView(Bundle savedInstanceState) {
 
-        homeFragment = new HomeFragment();
+        homeFragment = new HomeActivity();
         fragment02 = new Fragment02();
-        fragment03 = new Fragment03();
+        fragment01 = new Fragment01();
+        fragment04=new Fragment04();
         fragments = new ArrayList<>();
-        fragments.add(homeFragment);
+        fragments.add(fragment01);
         fragments.add(fragment02);
-        fragments.add(fragment03);
+        fragments.add(fragment04);
+
 
         adapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
         vpContent.setAdapter(adapter);
@@ -82,16 +81,25 @@ public class MainActivity extends BaseActivity {
     private void initNav() {
         tv_01 = (TextView) nvView.getHeaderView(0).findViewById(R.id.tv_01);
         tv_02 = (TextView) nvView.getHeaderView(0).findViewById(R.id.tv_02);
+        tv_03= (TextView) nvView.getHeaderView(0).findViewById(R.id.tv_03);
         tv_name = (TextView) nvView.getHeaderView(0).findViewById(R.id.tv_name);
 
         tv_01.setOnClickListener(new MyClickListener());
         tv_02.setOnClickListener(new MyClickListener());
+        tv_03.setOnClickListener(new MyClickListener());
     }
 
     private class MyClickListener implements View.OnClickListener{
 
         @Override
         public void onClick(View v) {
+            if (v==tv_01){
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }else if(v==tv_02){
+                //TODO 换肤
+            }else{
+                //TODO 退出
+            }
             dlLayout.closeDrawers();
         }
     }
